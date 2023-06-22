df7c7c054f9013beaadb2687136a7288e6c8aeba
Bruno <brunochelimo@gmail.com>
2023-06-22 15:10:12 +0300

    sms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-
 import './App.css';
 import Homepage from './components/Homepage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import React from 'react'
+import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <Homepage/>
-    </div>
-  )
+return (
+    <Router>
+          <div>
+              <Routes>
+                    <Route path="/" exact element={Homepage} />
+                    {/* Add more routes for other components */}
+            </Routes>
+          </div>
+    </Router>
+);
 }
 
-export default App
+export default App;
