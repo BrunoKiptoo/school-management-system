9fdcd8d5d62167ccfbd9484f5af83c426d0893f7
Bruno <brunochelimo@gmail.com>
2023-06-26 19:12:42 +0300

    sms

--- src/components/StudentManagement/StudentManagement.js
+++ src/components/StudentManagement/StudentManagement.js
@@ -111,11 +111,253 @@
 
 // export default StudentManagement;
 
+// import React, { useState, useEffect } from 'react';
+
+// function StudentManagement() {
+//   const [studentList, setStudentList] = useState([]);
+//   const [selectedStudent, setSelectedStudent] = useState(null);
+
+//   useEffect(() => {
+//     fetchStudentList();
+//   }, []);
+
+//   const fetchStudentList = async () => {
+//     try {
+//       const response = await fetch('http://localhost:3000/students');
+//       const data = await response.json();
+//       setStudentList(data);
+//     } catch (error) {
+//       console.error('Error fetching student list:', error);
+//     }
+//   };
+
+//   const handleStudentClick = (student) => {
+//     setSelectedStudent(student);
+//   };
+
+//   return (
+//     <div className="flex justify-center items-center h-screen bg-gray-900">
+//       {/* Student List Container */}
+//       <div className="w-1/3 pr-4">
+//         <h1 className="text-2xl font-semibold text-white mb-4">Student List</h1>
+//         <div className="space-y-4">
+//           {studentList && studentList.length > 0 ? (
+//             studentList.map((student) => (
+//               <div
+//                 key={student.id}
+//                 className="bg-purple-900 rounded-lg p-4 cursor-pointer hover:bg-purple-800"
+//                 onClick={() => handleStudentClick(student)}
+//               >
+//                 <h2 className="text-lg font-semibold text-white">{student.name}</h2>
+//                 <p className="text-sm text-gray-300">Grade: {student.grade}</p>
+//               </div>
+//             ))
+//           ) : (
+//             <p className="text-white">Loading student list...</p>
+//           )}
+//         </div>
+//       </div>
+
+//       {/* Modal Container */}
+//       <div className=" pl-4">
+//         {selectedStudent ? (
+//           <div className="bg-gray-800 rounded-lg p-4">
+//             <div className="flex items-center">
+//               <img
+//                 src={selectedStudent.image}
+//                 alt={selectedStudent.name}
+//                 className="w-1/2 h-auto mr-4"
+//               />
+//               <div className="w-1/2">
+//                 <h2 className="text-3xl font-semibold text-purple-400 mb-2">{selectedStudent.name}</h2>
+//                 <p className="text-lg text-gray-300">Grade: {selectedStudent.grade}</p>
+//                 <div className="mt-4">
+//                   <h3 className="text-lg font-semibold text-white">Guardians:</h3>
+//                   <ul className="pl-4 mt-1 space-y-2">
+//                     {selectedStudent.guardians.map((guardian, index) => (
+//                       <li key={index} className="text-base text-gray-300">
+//                         {guardian.name} - {guardian.relationship}
+//                       </li>
+//                     ))}
+//                   </ul>
+//                 </div>
+//                 <div className="mt-6">
+//                   <h3 className="text-lg font-semibold text-white">Profile:</h3>
+//                   <p className="text-base text-gray-300">
+//                     Age: {selectedStudent.profile.age} | Gender: {selectedStudent.profile.gender}
+//                   </p>
+//                   <p className="text-base text-gray-300">Address: {selectedStudent.profile.address}</p>
+//                   <p className="text-base text-gray-300">Phone: {selectedStudent.profile.phone}</p>
+//                 </div>
+//                 <div className="mt-6">
+//                   <h3 className="text-lg font-semibold text-white">Attendance:</h3>
+//                   <p className="text-base text-gray-300">
+//                     Days Present: {selectedStudent.attendance.daysPresent} | Days Absent:{' '}
+//                     {selectedStudent.attendance.daysAbsent} | Days Tardy: {selectedStudent.attendance.daysTardy}
+//                   </p>
+//                 </div>
+//                 <div className="mt-6">
+//                   <h3 className="text-lg font-semibold text-white">Grades:</h3>
+//                   <p className="text-base text-gray-300">
+//                     Math: {selectedStudent.grades.math} | Science: {selectedStudent.grades.science} | English:{' '}
+//                     {selectedStudent.grades.english}
+//                   </p>
+//                 </div>
+//                 <div className="mt-6">
+//                   <h3 className="text-lg font-semibold text-white">Discipline:</h3>
+//                   <p className="text-base text-gray-300">
+//                     Warnings: {selectedStudent.discipline.warnings} | Suspensions:{' '}
+//                     {selectedStudent.discipline.suspensions}
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         ) : (
+//           <p className="text-white">Select a student to view details.</p>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default StudentManagement;
+
+
+// import React, { useState, useEffect } from 'react';
+
+// function StudentManagement() {
+//   const [studentList, setStudentList] = useState([]);
+//   const [selectedStudent, setSelectedStudent] = useState(null);
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   useEffect(() => {
+//     fetchStudentList();
+//   }, []);
+
+//   const fetchStudentList = async () => {
+//     try {
+//       const response = await fetch('http://localhost:3000/students');
+//       const data = await response.json();
+//       setStudentList(data);
+//     } catch (error) {
+//       console.error('Error fetching student list:', error);
+//     }
+//   };
+
+//   const handleStudentClick = (student) => {
+//     setSelectedStudent(student);
+//   };
+
+//   const handleSearchChange = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   const filteredStudentList = studentList.filter((student) =>
+//     student.name.toLowerCase().includes(searchTerm.toLowerCase())
+//   );
+
+//   return (
+//     <div className="flex justify-center items-center h-screen bg-gray-900">
+//       {/* Student List Container */}
+//       <div className="w-1/3 pr-4">
+//         <h1 className="text-2xl font-semibold text-white mb-4">Student List</h1>
+//         <div className="flex items-center mb-4">
+//           <input
+//             type="text"
+//             placeholder="Search by name"
+//             className="p-2 rounded-l-lg w-full bg-white text-gray-800 focus:outline-none"
+//             value={searchTerm}
+//             onChange={handleSearchChange}
+//           />
+//           <button className="bg-purple-900 rounded-r-lg px-4 py-2 text-white font-semibold hover:bg-purple-800 focus:outline-none">
+//             Search
+//           </button>
+//         </div>
+//         <div className="space-y-4">
+//           {filteredStudentList.length > 0 ? (
+//             filteredStudentList.map((student) => (
+//               <div
+//                 key={student.id}
+//                 className="bg-purple-900 rounded-lg p-4 cursor-pointer hover:bg-purple-800"
+//                 onClick={() => handleStudentClick(student)}
+//               >
+//                 <h2 className="text-lg font-semibold text-white">{student.name}</h2>
+//                 <p className="text-sm text-gray-300">Grade: {student.grade}</p>
+//               </div>
+//             ))
+//           ) : (
+//             <p className="text-white">No students found.</p>
+//           )}
+//         </div>
+//       </div>
+
+//       {/* Modal Container */}
+//       <div className="pl-4">
+//         {selectedStudent ? (
+//           <div className="bg-gray-800 rounded-lg p-4">
+//             <div className="flex items-center">
+//               <img src={selectedStudent.image} alt={selectedStudent.name} className="w-1/2 h-auto mr-4" />
+//               <div className="w-1/2">
+//                 <h2 className="text-3xl font-semibold text-purple-400 mb-2">{selectedStudent.name}</h2>
+//                 <p className="text-lg text-gray-300">Grade: {selectedStudent.grade}</p>
+//                 <div className="mt-4">
+//                   <h3 className="text-lg font-semibold text-white">Guardians:</h3>
+//                   <ul className="pl-4 mt-1 space-y-2">
+//                     {selectedStudent.guardians.map((guardian, index) => (
+//                       <li key={index} className="text-base text-gray-300">
+//                         {guardian.name} - {guardian.relationship}
+//                       </li>
+//                     ))}
+//                   </ul>
+//                 </div>
+//                 <div className="mt-6">
+//                   <h3 className="text-lg font-semibold text-white">Profile:</h3>
+//                   <p className="text-base text-gray-300">
+//                     Age: {selectedStudent.profile.age} | Gender: {selectedStudent.profile.gender}
+//                   </p>
+//                   <p className="text-base text-gray-300">Address: {selectedStudent.profile.address}</p>
+//                   <p className="text-base text-gray-300">Phone: {selectedStudent.profile.phone}</p>
+//                 </div>
+//                 <div className="mt-6">
+//                   <h3 className="text-lg font-semibold text-white">Attendance:</h3>
+//                   <p className="text-base text-gray-300">
+//                     Days Present: {selectedStudent.attendance.daysPresent} | Days Absent:{' '}
+//                     {selectedStudent.attendance.daysAbsent} | Days Tardy: {selectedStudent.attendance.daysTardy}
+//                   </p>
+//                 </div>
+//                 <div className="mt-6">
+//                   <h3 className="text-lg font-semibold text-white">Grades:</h3>
+//                   <p className="text-base text-gray-300">
+//                     Math: {selectedStudent.grades.math} | Science: {selectedStudent.grades.science} | English:{' '}
+//                     {selectedStudent.grades.english}
+//                   </p>
+//                 </div>
+//                 <div className="mt-6">
+//                   <h3 className="text-lg font-semibold text-white">Discipline:</h3>
+//                   <p className="text-base text-gray-300">
+//                     Warnings: {selectedStudent.discipline.warnings} | Suspensions:{' '}
+//                     {selectedStudent.discipline.suspensions}
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         ) : (
+//           <p className="text-white">Select a student to view details.</p>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default StudentManagement;
 import React, { useState, useEffect } from 'react';
 
 function StudentManagement() {
   const [studentList, setStudentList] = useState([]);
   const [selectedStudent, setSelectedStudent] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetchStudentList();
@@ -135,17 +377,37 @@ function StudentManagement() {
     setSelectedStudent(student);
   };
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredStudentList = studentList.filter((student) =>
+    student.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="flex justify-center items-center h-screen bg-gray-900">
       {/* Student List Container */}
       <div className="w-1/3 pr-4">
-        <h1 className="text-2xl font-semibold text-white mb-4">Student List</h1>
-        <div className="space-y-4">
-          {studentList && studentList.length > 0 ? (
-            studentList.map((student) => (
+        <h1 className="text-3xl font-semibold text-white mb-4">Student List</h1>
+        <div className="flex items-center mb-4">
+          <input
+            type="text"
+            placeholder="Search by name"
+            className="p-2 rounded-l-lg w-full bg-gray-800 text-white focus:outline-none"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <button className="bg-purple-900 rounded-r-lg px-4 py-2 text-white font-semibold hover:bg-purple-800 focus:outline-none">
+            Search
+          </button>
+        </div>
+        <div className="max-h-96 overflow-y-auto">
+          {filteredStudentList.length > 0 ? (
+            filteredStudentList.map((student) => (
               <div
                 key={student.id}
-                className="bg-purple-900 rounded-lg p-4 cursor-pointer hover:bg-purple-800"
+                className="bg-gray-800 rounded-lg p-4 cursor-pointer hover:bg-purple-800 mb-4"
                 onClick={() => handleStudentClick(student)}
               >
                 <h2 className="text-lg font-semibold text-white">{student.name}</h2>
@@ -153,21 +415,17 @@ function StudentManagement() {
               </div>
             ))
           ) : (
-            <p className="text-white">Loading student list...</p>
+            <p className="text-white">No students found.</p>
           )}
         </div>
       </div>
 
       {/* Modal Container */}
-      <div className=" pl-4">
+      <div className="pl-4">
         {selectedStudent ? (
           <div className="bg-gray-800 rounded-lg p-4">
             <div className="flex items-center">
-              <img
-                src={selectedStudent.image}
-                alt={selectedStudent.name}
-                className="w-1/2 h-auto mr-4"
-              />
+              <img src={selectedStudent.image} alt={selectedStudent.name} className="w-1/2 h-auto mr-4" />
               <div className="w-1/2">
                 <h2 className="text-3xl font-semibold text-purple-400 mb-2">{selectedStudent.name}</h2>
                 <p className="text-lg text-gray-300">Grade: {selectedStudent.grade}</p>
